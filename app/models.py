0f35846a814ce3f436740a9f79b652e2d048e375
Michal Blaha <michal@michalblaha.cz>
2023-12-09 23:38:16 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,80 +1,90 @@
 from pydantic import BaseModel, validator
 from typing import Optional, List, Union
 
-class Response(BaseModel):
+class NoValidationBaseModel(BaseModel):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        return v  # simply return the value without validation
+
+
+class Response(NoValidationBaseModel):
     identifier: str
     message: str
 
 
-class Metadata(BaseModel):
+class Metadata(NoValidationBaseModel):
     task_type: str
     file_name: Optional[str]
     duration: Optional[float]
 
 
-class Task(BaseModel):
+class Task(NoValidationBaseModel):
     identifier: str
     status: str
     task_type: str
 
 
-class ResultTasks(BaseModel):
+class ResultTasks(NoValidationBaseModel):
     tasks: List[Task]
 
 
-class TranscriptionSegment(BaseModel):
+class TranscriptionSegment(NoValidationBaseModel):
     start: float
     end: float
     text: str
 
 
-class Segment(BaseModel):
+class Segment(NoValidationBaseModel):
     start: float
     end: float
     text: Optional[str]
     speaker: Optional[str]
 
 
-class Word(BaseModel):
+class Word(NoValidationBaseModel):
     word: str
     start: float
     end: float
     score: float
 
 
-class AlingmentSegment(BaseModel):
+class AlingmentSegment(NoValidationBaseModel):
     start: float
     end: float
     text: str
     words: List[Word]
 
 
-class AlignedTranscription(BaseModel):
+class AlignedTranscription(NoValidationBaseModel):
     segments: List[AlingmentSegment]
     word_segments: List[Word]
 
 
-class DiarizationSegment(BaseModel):
+class DiarizationSegment(NoValidationBaseModel):
     label: str
     speaker: str
     start: float
     end: float
 
 
-class DiaredTrancript(BaseModel):
+class DiaredTrancript(NoValidationBaseModel):
     segments: List[Segment]
 
 
-class Transcript(BaseModel):
+class Transcript(NoValidationBaseModel):
     segments: List[TranscriptionSegment]
     language: Optional[str]
 
 
-class TranscriptInput(BaseModel):
+class TranscriptInput(NoValidationBaseModel):
     transcript: Transcript
 
 
-class Result(BaseModel):
+class Result(NoValidationBaseModel):
     status: str
     result: Optional[
         Union[
@@ -86,8 +96,3 @@ class Result(BaseModel):
     ]
     metadata: Metadata
     error: Optional[str]
-
-    @validator('result', pre=True)
-    def bypass_standard_validation(cls, value):
-        # Bypass or customize the validation
-        return value
